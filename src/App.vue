c9005e9bcfa5afbb64dbbf5ebafe5c6c4ce18369
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-27 13:09:09 +0100

    Get rid of the clutter
    It's still accessible in the console anyways

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,9 @@
 import { ref } from "vue";
 import { useCloudlinkStore } from "./stores/cloudlink";
 import Login from "./components/Login.vue";
-
-const cloudlinkStore = useCloudlinkStore();
-
-const packets = ref<unknown[]>([]);
-cloudlinkStore.cloudlink.on("packet", (packet: unknown) => {
-  packets.value.push(packet);
-});
 </script>
 
 <template>
   <h1>Roarer</h1>
   <Login />
-  <h2>Packets:</h2>
-  <ul>
-    <li v-for="packet in packets">{{ JSON.stringify(packet) }}</li>
-  </ul>
 </template>
