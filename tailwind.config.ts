35f4a00113b1c84c8e341a883c53e9e314f6db37
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-28 13:09:36 +0100

    Use preferred monospace font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: "monospace",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
