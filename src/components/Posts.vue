e42eb83963e45b44cad5c25dffa70a4972ad4f4a
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-28 18:17:39 +0100

    Move post logic into its own component

---
+++ src/components/Posts.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import EnterPost from "./EnterPost.vue";
+import TypingIndicator from "./TypingIndicator.vue";
+import OnlineList from "./OnlineList.vue";
+import Post from "./Post.vue";
+import { useCloudlinkStore } from "../stores/cloudlink";
+import { postSchema } from "../lib/postSchema";
+import { z } from "zod";
+
+const cloudlinkStore = useCloudlinkStore();
+
+const homePosts = ref<z.infer<typeof postSchema>[]>([]);
+
+const homeSchema = z.object({
+  autoget: postSchema.array(),
+});
+(async () => {
+  homePosts.value = homeSchema.parse(
+    await (await fetch("https://api.meower.org/home?autoget=1")).json(),
+  ).autoget;
+
+  const newPostSchema = z.object({
+    cmd: z.literal("direct"),
+    val: postSchema,
+  });
+  cloudlinkStore.lookFor(
+    newPostSchema,
+    ({ val: post }) => {
+      homePosts.value.unshift(post);
+      homePosts.value = homePosts.value;
+    },
+    false,
+  );
+})();
+
+const enterPost = ref<InstanceType<typeof EnterPost> | null>(null);
+</script>
+
+<template>
+  <OnlineList />
+  <EnterPost ref="enterPost" />
+  <TypingIndicator />
+  <Post
+    :post="post"
+    :key="post.post_id"
+    @reply="enterPost?.reply"
+    v-for="post in homePosts"
+  />
+</template>
