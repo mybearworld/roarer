3e12d57b6bcba6b0bf0fec3fb65af1c0789b9873
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-28 23:51:19 +0100

    Get rid of the console.log

--- src/components/EnterPost.vue
+++ src/components/EnterPost.vue
@@ -46,7 +46,6 @@ const post = async (e?: Event) => {
 
 const trimmedPost = (post: string) => {
   post = post.replace(/\[/g, "{").replace(/\]/g, "}");
-  console.log(post);
   const quoteMatch = post.match(/^@[a-z_-]+(?: \[.{0,40}…?\] )?(.*)$/i);
   const postContent = quoteMatch ? quoteMatch[1] : post;
   return `${postContent.slice(0, 40).trim()}${
