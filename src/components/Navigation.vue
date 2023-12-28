486f5e1eef8b5ebf4c1d3264c8f6fdb86d132c3e
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-28 19:12:33 +0100

    Add note on status

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -12,22 +12,28 @@ const locationStore = useLocationStore();
 </script>
 
 <template>
-  <div class="space-x-2">
-    <h1 class="inline-block text-3xl font-bold">Roarer - {{ title }}</h1>
-    <Login />
-    <div class="float-right space-x-2">
-      <button
-        class="text-sky-400 underline"
-        @click="locationStore.location = 'home'"
-      >
-        Home
-      </button>
-      <button
-        class="text-sky-400 underline"
-        @click="locationStore.location = 'group'"
-      >
-        Groups
-      </button>
+  <div>
+    <div class="space-x-2">
+      <h1 class="inline-block text-3xl font-bold">Roarer - {{ title }}</h1>
+      <Login />
+      <div class="float-right space-x-2">
+        <button
+          class="text-sky-400 underline"
+          @click="locationStore.location = 'home'"
+        >
+          Home
+        </button>
+        <button
+          class="text-sky-400 underline"
+          @click="locationStore.location = 'group'"
+        >
+          Groups
+        </button>
+      </div>
     </div>
+    <p>
+      Note: Roarer is very early in its development. Please do report bugs
+      and/or suggest features!
+    </p>
   </div>
 </template>
