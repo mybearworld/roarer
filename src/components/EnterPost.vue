c29a3ac5690f7a063a8ec82c320d45a45feb52f3
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-12-29 16:07:07 +0100

    Remove new lines in replies

--- src/components/EnterPost.vue
+++ src/components/EnterPost.vue
@@ -54,7 +54,10 @@ const post = async (e?: Event) => {
 const trimmedPost = (post: string) => {
   const quoteMatch = post.match(/^@[a-z_-]+(?: \[.{0,40}…?\] )?(.*)$/i);
   const postContent = quoteMatch ? quoteMatch[1] : post;
-  const replacedPostContent = postContent.replace("[", "{").replace("]", "}");
+  const replacedPostContent = postContent
+    .replace("[", "{")
+    .replace("]", "}")
+    .replace("\n", " ");
   return `${replacedPostContent.slice(0, 40).trim()}${
     postContent.length > 39 ? "…" : ""
   }`;
