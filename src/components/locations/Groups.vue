d1631e921b0f0acda7bcbae83255e6a8ebee11b5
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-02 11:34:44 +0100

    Add creating group chats

--- src/components/locations/Groups.vue
+++ src/components/locations/Groups.vue
@@ -32,6 +32,41 @@ effect(async () => {
   chats.value = response.autoget;
 });
 
+const chatNickname = ref<HTMLInputElement | null>(null);
+const createChat = async (e?: Event) => {
+  e?.preventDefault();
+  const { username, token } = loginStatusStore;
+  if (username === null || token === null || chatNickname.value === null) {
+    return;
+  }
+  const response = await fetch("https://api.meower.org/chats", {
+    method: "POST",
+    headers: { username, token, "content-type": "application/json" },
+    body: JSON.stringify({
+      nickname: chatNickname.value.value,
+    }),
+  });
+  if (response.status !== 200) {
+    alert(`Failed creating chat: ${response.status}`);
+  }
+};
+
+const newChatSchema = z.object({
+  cmd: z.literal("direct"),
+  val: z.object({
+    mode: z.literal("create_chat"),
+    payload: chatSchema,
+  }),
+});
+cloudlinkStore.lookFor(
+  newChatSchema,
+  (packet) => {
+    chats.value.push(packet.val.payload);
+    chats.value = chats.value;
+  },
+  false,
+);
+
 const openGroupchat = ref<APIChat | null>(null);
 const open = (chat: APIChat) => {
   openGroupchat.value = chat;
@@ -41,12 +76,20 @@ const open = (chat: APIChat) => {
 <template>
   <div class="block space-y-2">
     <Navigation title="Groups" />
-    <ChatView
-      :chat="chat"
-      @open="open"
-      v-for="chat in chats"
-      v-if="openGroupchat === null"
-    />
+    <template v-if="openGroupchat === null">
+      <form class="flex gap-2" @submit="createChat">
+        <input
+          type="text"
+          placeholder="Nickname"
+          class="w-full rounded-lg bg-slate-800 px-2 py-1"
+          ref="chatNickname"
+        />
+        <button type="submit" class="rounded-xl bg-slate-800 px-2 py-1">
+          Create
+        </button>
+      </form>
+      <ChatView :chat="chat" @open="open" v-for="chat in chats" />
+    </template>
     <Posts :chat="openGroupchat" v-else />
   </div>
 </template>
