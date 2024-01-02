4abe7cfde155a0ae4c28f9a8ebc8d1469fbeaa5e
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-02 13:18:44 +0100

    Make server posts italic

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -332,7 +332,11 @@ effect(() => {
     v-if="!isDeleted"
   >
     <div class="relative flex flex-wrap items-center gap-x-2">
-      <button class="font-bold" @click="goToUser(username)">
+      <button
+        v-if="post.u !== 'Server'"
+        class="font-bold"
+        @click="goToUser(username)"
+      >
         {{ username }}
       </button>
       <span
@@ -428,7 +432,9 @@ effect(() => {
     </form>
     <div v-else>
       <div
-        class="max-h-96 space-y-2 overflow-y-auto break-words [&_a]:text-sky-400 [&_a]:underline [&_blockquote]:border-l-2 [&_blockquote]:border-slate-500 [&_blockquote]:pl-2 [&_blockquote]:italic [&_blockquote]:text-slate-400 [&_h1]:text-4xl [&_h1]:font-bold [&_h2]:text-3xl [&_h2]:font-bold [&_h3]:text-2xl [&_h3]:font-bold [&_h4]:text-xl [&_h4]:font-bold [&_h5]:text-lg [&_h5]:font-bold [&_h6]:text-sm [&_h6]:font-bold [&_hr]:mx-8 [&_hr]:my-2 [&_hr]:border-slate-500 [&_img]:max-h-96 [&_li]:list-inside [&_ol_li]:list-decimal [&_td]:border-[1px] [&_td]:border-slate-500 [&_td]:px-2 [&_td]:py-1 [&_th]:border-[1px] [&_th]:border-slate-500 [&_th]:px-2 [&_th]:py-1 [&_ul_li]:list-disc"
+        :class="`max-h-96 space-y-2 overflow-y-auto break-words [&_a]:text-sky-400 [&_a]:underline [&_blockquote]:border-l-2 [&_blockquote]:border-slate-500 [&_blockquote]:pl-2 [&_blockquote]:italic [&_blockquote]:text-slate-400 [&_h1]:text-4xl [&_h1]:font-bold [&_h2]:text-3xl [&_h2]:font-bold [&_h3]:text-2xl [&_h3]:font-bold [&_h4]:text-xl [&_h4]:font-bold [&_h5]:text-lg [&_h5]:font-bold [&_h6]:text-sm [&_h6]:font-bold [&_hr]:mx-8 [&_hr]:my-2 [&_hr]:border-slate-500 [&_img]:max-h-96 [&_li]:list-inside [&_ol_li]:list-decimal [&_td]:border-[1px] [&_td]:border-slate-500 [&_td]:px-2 [&_td]:py-1 [&_th]:border-[1px] [&_th]:border-slate-500 [&_th]:px-2 [&_th]:py-1 [&_ul_li]:list-disc ${
+          post.u === 'Server' ? 'italic' : ''
+        }`"
         v-html="markdownPostContent"
         ref="postContentElement"
       ></div>
