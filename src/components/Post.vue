cd1a1e981b561abf0240be2b02479f84232e9e44
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-13 19:38:26 +0100

    Show post after cancellation

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -358,13 +358,14 @@ const reload = () => location.reload();
         </button>
         <button
           class="rounded-xl bg-slate-700 px-2 py-1"
+          type="button"
           @click="editing = false"
         >
           {{ t("cancelEditingPost") }}
         </button>
       </div>
     </form>
-    <div v-else>
+    <div :class="editing ? 'hidden' : ''">
       <div
         :class="`max-h-96 space-y-2 break-words [&_a]:text-sky-400 [&_a]:underline [&_blockquote]:border-l-2 [&_blockquote]:border-slate-500 [&_blockquote]:pl-2 [&_blockquote]:italic [&_blockquote]:text-slate-400 [&_h1]:text-4xl [&_h1]:font-bold [&_h2]:text-3xl [&_h2]:font-bold [&_h3]:text-2xl [&_h3]:font-bold [&_h4]:text-xl [&_h4]:font-bold [&_h5]:text-lg [&_h5]:font-bold [&_h6]:text-sm [&_h6]:font-bold [&_hr]:mx-8 [&_hr]:my-2 [&_hr]:border-slate-500 [&_img]:max-h-96 [&_img]:align-top [&_li]:list-inside [&_ol_li]:list-decimal [&_td]:border-[1px] [&_td]:border-slate-500 [&_td]:px-2 [&_td]:py-1 [&_th]:border-[1px] [&_th]:border-slate-500 [&_th]:px-2 [&_th]:py-1 [&_ul_li]:list-disc [&_video]:max-h-96 ${
           isItalicUser ? 'italic' : ''
