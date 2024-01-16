64804240423667ba1203c60058d8f6a9beaadf6c
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-16 14:54:55 +0100

    Organize settings store better

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -8,15 +8,16 @@ export const useSettingsStore = defineStore("settings", () => {
   const anyImageHost = ref(
     localStorage.getItem(ANY_IMG_HOST_STORAGE) === "true",
   );
+  const filterSwears = ref(
+    ["true", null].includes(localStorage.getItem(FILTER_SWEARS_STORAGE)),
+  );
+
   effect(() => {
     localStorage.setItem(
       ANY_IMG_HOST_STORAGE,
       anyImageHost.value ? "true" : "false",
     );
   });
-  const filterSwears = ref(
-    ["true", null].includes(localStorage.getItem(FILTER_SWEARS_STORAGE)),
-  );
   effect(() => {
     localStorage.setItem(
       FILTER_SWEARS_STORAGE,
