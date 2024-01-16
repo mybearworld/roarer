956a90790c526e77fea2176b45bd799541aee7b7
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-01-16 15:50:15 +0100

    Use `ulist` instead of trying to use `get_ulist`

--- src/stores/onlinelist.ts
+++ src/stores/onlinelist.ts
@@ -4,16 +4,15 @@ import { z } from "zod";
 import { useCloudlinkStore } from "./cloudlink";
 
 export const useOnlinelistStore = defineStore("onlinelist", () => {
-  const online = ref<string[]>([]);
-
   const cloudlinkStore = useCloudlinkStore();
-  cloudlinkStore.send(
-    {
-      cmd: "get_ulist",
-      val: null,
-    },
-    z.object({}),
+
+  const online = ref<string[]>(
+    cloudlinkStore.cloudlink.ulist
+      ? // the type of ulist is phenomenally awful
+        (cloudlinkStore.cloudlink.ulist as unknown as string).split(";")
+      : [],
   );
+
   cloudlinkStore.cloudlink.on("ulist", (packet: unknown) => {
     const newOnline = z.object({ val: z.string() }).parse(packet);
     online.value = newOnline.val.split(";").slice(0, -1);
