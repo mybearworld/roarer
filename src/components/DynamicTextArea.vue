05335455fb5459af68cc459655908c6e7ed6be9d
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-04-14 10:52:11 +0200

    Resize DynamicTextArea when it is created

--- src/components/DynamicTextArea.vue
+++ src/components/DynamicTextArea.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { effect, ref, watch } from "vue";
 
 const model = defineModel<string>();
 const el = ref<HTMLTextAreaElement>();
 
-watch([model], () => {
+const resize = () => {
   requestAnimationFrame(() => {
     if (!el.value) return;
     el.value.style.height = "auto";
     el.value.style.height = `${el.value.scrollHeight + 4}px`;
   });
+};
+
+watch([model], () => {
+  resize();
+});
+
+effect(() => {
+  if (!el.value) return;
+  resize();
 });
 </script>
 
