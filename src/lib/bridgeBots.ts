8cf5a276044dd9ad185d5aad29dbf46557d60764
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-05-20 17:54:00 +0200

    Mark gc as a bridge

--- src/lib/bridgeBots.ts
+++ src/lib/bridgeBots.ts
@@ -1,1 +1,7 @@
-export const bridgeBots = ["Discord", "Webhooks", "RevowerJS", "SplashBridge"];
+export const bridgeBots = [
+  "Discord",
+  "Webhooks",
+  "RevowerJS",
+  "SplashBridge",
+  "gc",
+];
